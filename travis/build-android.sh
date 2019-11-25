864b68f343dec13ba229d04a67b46068213f872a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-25 09:15:38 +0000

    Deploy ShilpeshPatel/IonicTravish to github.com/ShilpeshPatel/IonicTravish.git:gh-pages

--- travis/build-android.sh
+++ travis/build-android.sh
@@ -9,5 +9,6 @@ if [[ "$TRAVIS_BRANCH" == "develop" ]]
 then
     ionic cordova build android
 else
-    ionic cordova build android --prod --release
+    ionic cordova build android
+    # ionic cordova build android --prod --release
 fi
